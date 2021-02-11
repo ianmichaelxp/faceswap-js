717bd6f77898ae5705ee503400de416dbda4ed4e
ianmichael <ianmichael.xp@gmail.com>
2021-02-11 09:01:55 -0300

    http

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const https = require('https')
+// const https = require('https')
+//     ,fs = require("fs")
+//     ,app = require('./config/express');
+
+// const options = {
+//     key: fs.readFileSync("certificado.key"),
+//     cert: fs.readFileSync("certificado.cert")
+//     };
+
+// https.createServer(options, app).listen(5000, function() {
+//     console.log('Servidor estutando na porta: ' + this.address().port);
+// });
+
+const http = require('http')
     ,fs = require("fs")
     ,app = require('./config/express');
 
@@ -7,6 +20,6 @@ const options = {
     cert: fs.readFileSync("certificado.cert")
     };
 
-https.createServer(options, app).listen(5000, function() {
+http.createServer(app).listen(5000, function() {
     console.log('Servidor estutando na porta: ' + this.address().port);
 });
