58f065201cf417c880968abcb9a5e114ebfbfc95
ianmichael <ianmichael.xp@gmail.com>
2021-02-11 09:59:18 -0300

    deploy https

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const https = require('https')
-//     ,fs = require("fs")
-//     ,app = require('./config/express');
-
-// const options = {
-//     key: fs.readFileSync("certificado.key"),
-//     cert: fs.readFileSync("certificado.cert")
-//     };
-
-// https.createServer(options, app).listen(5000, function() {
-//     console.log('Servidor estutando na porta: ' + this.address().port);
-// });
-
 const http = require('http')
     ,fs = require("fs")
     ,app = require('./config/express');
