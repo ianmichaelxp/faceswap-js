d6acb89d18c238aa9b3aec2322801941f80557b5
ianmichael <ianmichael.xp@gmail.com>
2021-02-11 09:46:39 -0300

    Procfile

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ const options = {
     cert: fs.readFileSync("certificado.cert")
     };
 
-http.createServer(app).listen(5000, function() {
-    console.log('Servidor estutando na porta: ' + this.address().port);
+http.createServer(app).listen(process.env.PORT || 5000, function() {
+    console.log('Servidor escutando na porta: ' + this.address().port);
 });
